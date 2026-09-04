f6fe8f2ea0840631baa4cf930dcffbc11794a5d2
Rohit <rrohit2911@gmail.com>
2026-09-04 12:20:22 +0530

    create user migrations

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -11,11 +11,11 @@ from app.core.database import Base
 from app.core.config import settings
 from app.models.user import User
 
-target_metadata = Base.metadata
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -26,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
